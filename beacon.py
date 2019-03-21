cab75f41f7e2841fd788acbf44c397389bd229d3
argaon <arttempler@naver.com>
2019-03-21 20:22:09 +0900

    refs #2 ios bug fix

--- beacon.py
+++ beacon.py
@@ -24,8 +24,21 @@ class makebeacon:
 
     def sendbeacon(self):
         essid = Dot11Elt(ID="SSID", info=self.mssid)
-        sendp(RadioTap()/self.dot11/self.beacon11/essid, iface=self.interface, loop=0, verbose=False)
-
+        sr = Dot11Elt(ID="Rates", info="\x01\x08\x82\x84\x8b\x96\x0c\x12\x18\x23")
+        ds = Dot11Elt(ID="DSset", info="\x03")
+        tim = Dot11Elt(ID="TIM", info="\x00\x01\x00\x00")
+        erp = Dot11Elt(ID="ERPinfo", info="\x2a")
+        rsn = Dot11Elt(ID='RSNinfo', info=(
+        '\x01\x00'                 #RSN Version 1
+        '\x00\x0f\xac\x02'         #Group Cipher Suite : 00-0f-ac TKIP
+        '\x02\x00'                 #2 Pairwise Cipher Suites (next two lines)
+        '\x00\x0f\xac\x04'         #AES Cipher
+        '\x00\x0f\xac\x02'         #TKIP Cipher
+        '\x01\x00'                 #1 Authentication Key Managment Suite (line below)
+        '\x00\x0f\xac\x02'         #Pre-Shared Key
+        '\x00\x00'))
+        #sendp(RadioTap()/self.dot11/self.beacon11/essid, iface=self.interface, loop=0, verbose=False)
+        sendp(RadioTap()/self.dot11/self.beacon11/essid/sr/ds/tim/erp/rsn, iface=self.interface, loop=0, verbose=False)
 class Multibeacon(Process):
     global RUNNING
     def __init__(self):
